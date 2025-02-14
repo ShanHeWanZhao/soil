37b64d65df18d28d50749237e1c20c50b7a797b9
reef <shanzhao.rd@gmail.com>
2025-02-14 22:48:39 +0800

    leetcode：随机链表的复制 & 排序链表

---
+++ src/main/java/com/github/soil/algorithm/leetcode/editor/cn/Node.java
@@ -0,0 +1,13 @@
+package com.github.soil.algorithm.leetcode.editor.cn;
+
+public class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
